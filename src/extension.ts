33a7008a437545ef0a7206fa8f4a545bd0fd5a93
dubisdev <dubisdev@gmail.com>
2022-05-04 14:33:51 +0200

    feat(command-default): set and launch default app

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,12 @@
-import { commands, window, ExtensionContext, Uri, env } from "vscode";
+import {
+  commands,
+  window,
+  ExtensionContext,
+  Uri,
+  env,
+  workspace,
+  ProgressLocation,
+} from "vscode";
 
 // @ts-ignore
 import getApps from "installed-sw";
@@ -6,29 +14,55 @@ import getApps from "installed-sw";
 const makeAppsQPIs = (apps: any[]) =>
   apps.map((app) => ({ ...app, label: app.name }));
 
-const listApps = async () => {
+const createAppsQuickPick = async ({ title = "🚀 SELECT APP 🚀" } = {}) => {
   const apps = await getApps();
-
   let quickPickItems = makeAppsQPIs(apps);
   const quickPick = window.createQuickPick();
+  quickPick.title = title;
   quickPick.items = quickPickItems;
-
-  quickPick.onDidChangeSelection((items) => {
-    // @ts-ignore
-    const itemsPaths = items.map((item) => item.path);
-    const itemPath = itemsPaths[0];
-
-    env.openExternal(Uri.parse(itemPath));
-  });
   quickPick.onDidHide(() => quickPick.dispose());
-  quickPick.show();
+
+  return quickPick;
 };
 
-// TODO: Add a command to select default app and run it
-// const launchDefaultApp = async () => {
-//   const config = workspace.getConfiguration("defaultApp");
-//   env.openExternal(Uri.parse("https://www.google.com"));
-// };
+const launchApp = async (appQPI: any) => {
+  return await env.openExternal(Uri.parse(appQPI.path));
+};
+
+const listApps = async () => {
+  const appsQuickPick = await createAppsQuickPick();
+
+  appsQuickPick.onDidChangeSelection((items) => {
+    const item = items[0];
+    launchApp(item);
+  });
+  appsQuickPick.show();
+};
+
+const setDefaultApp = async () => {
+  const appsQuickPick = await createAppsQuickPick({
+    title: "🚀 SELECT DEFAULT APP 🚀",
+  });
+
+  appsQuickPick.onDidChangeSelection((items) => {
+    // @ts-ignore
+    const itemPath = items[0].path;
+
+    workspace.getConfiguration("launcher").update("defaultApp", itemPath);
+    launchApp(items[0]);
+  });
+
+  appsQuickPick.show();
+};
+
+const launchDefaultApp = async () => {
+  const defaultAppPath = workspace
+    .getConfiguration("launcher")
+    .get<string>("defaultApp");
+
+  if (!defaultAppPath) return await setDefaultApp();
+  else launchApp({ path: defaultAppPath, label: "Default App" });
+};
 
 export function activate(context: ExtensionContext) {
   const listAppsSubscription = commands.registerCommand(
@@ -36,10 +70,10 @@ export function activate(context: ExtensionContext) {
     listApps
   );
 
-  // const launchDefaultAppSubscription = commands.registerCommand(
-  //   `extension.launchDefaultApp`,
-  //   launchDefaultApp
-  // );
+  const launchDefaultAppSubscription = commands.registerCommand(
+    `launcher.launchDefaultApp`,
+    launchDefaultApp
+  );
   context.subscriptions.push(listAppsSubscription);
-  //context.subscriptions.push(launchDefaultAppSubscription);
+  context.subscriptions.push(launchDefaultAppSubscription);
 }
